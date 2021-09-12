947f6b03500d15fc2d7aedec9a469c28fda37993
Sanjay Kenn <sanjaykenn@gmail.com>
2021-09-12 21:52:24 +0200

    Adding 2015/4

---
+++ 2015/4/part-2/main.py
@@ -0,0 +1,16 @@
+import hashlib
+import itertools
+
+
+def main(inp):
+	key = inp.rstrip('\n')
+
+	for i in itertools.count(1):
+		if hashlib.md5(f'{key}{i}'.encode()).hexdigest().startswith('000000'):
+			return i
+
+
+if __name__ == '__main__':
+	import sys
+
+	print(main(''.join(sys.stdin.readlines())))
