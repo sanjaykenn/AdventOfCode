338ce62c90c2b6fb9814d8e578bb150bf96b17d3
Sanjay Kenn <sanjaykenn@gmail.com>
2021-09-13 20:30:08 +0200

    Adding 2015/9

---
+++ 2015/9/part-1/main.py
@@ -0,0 +1,20 @@
+import itertools
+import re
+
+from graph import Graph
+
+
+def main(inp):
+	re_distance = re.compile('^(.*) to (.*) = (\\d*)$')
+	g = Graph()
+
+	for line in inp[:-1].split('\n'):
+		location1, location2, distance = re_distance.match(line).groups()
+		g.add_edge(location1, location2, int(distance), True)
+
+	return min(map(g.distance_from_path, itertools.permutations(g.nodes())))
+
+
+if __name__ == '__main__':
+	import sys
+	print(main(''.join(sys.stdin.readlines())))
