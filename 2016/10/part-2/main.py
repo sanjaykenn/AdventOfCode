05211b7f4216fa0ad82cf60d30da993652c7cd3a
Sanjay Kenn <sanjaykenn@gmail.com>
2021-09-25 17:08:29 +0200

    Adding 2016/10

---
+++ 2016/10/part-2/main.py
@@ -0,0 +1,52 @@
+import re
+
+
+def main(inp):
+	re_goes = re.compile('^value (\\d+) goes to bot (\\d+)$')
+	re_give = re.compile('^bot (\\d+) gives low to ([a-z]+) (\\d+) and high to ([a-z]+) (\\d+)$')
+	bots = {}
+	outputs = {}
+	give_commands = []
+
+	for line in inp.rstrip('\n').split('\n'):
+		m = re_goes.match(line)
+		if m is not None:
+			value, bot = map(int, m.groups())
+			bots[bot] = bots.get(bot, ()) + (value, )
+			continue
+
+		m = re_give.match(line)
+		if m is not None:
+			bot_give, id1, out1, id2, out2 = m.groups()
+			bot_give = int(bot_give)
+			out1 = int(out1)
+			out2 = int(out2)
+			give_commands.append((bot_give, id1, out1, id2, out2))
+
+	while True:
+		change = False
+		for bot_give, id1, out1, id2, out2 in give_commands:
+			if len(bots.get(bot_give, ())) != 2:
+				continue
+
+			change = True
+			low, high = sorted(bots[bot_give])
+
+			bots[bot_give] = ()
+			if id1 == 'bot':
+				bots[out1] = bots.get(out1, ()) + (low, )
+			else:
+				outputs[out1] = low
+
+			if id2 == 'bot':
+				bots[out2] = bots.get(out2, ()) + (high, )
+			else:
+				outputs[out2] = high
+
+		if not change:
+			return outputs[0] * outputs[1] * outputs[2]
+
+
+if __name__ == '__main__':
+	import sys
+	print(main(''.join(sys.stdin.readlines())))
