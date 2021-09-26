e0e7c4173aae4beb0f03e4a6af7d167b8fe2a427
Sanjay Kenn <sanjaykenn@gmail.com>
2021-09-26 02:11:26 +0200

    Adding 2016/16

---
+++ 2016/16/part-2/main.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def main(inp):
+	a = np.array(list(inp.rstrip('\n'))) == '1'
+	length = 35651584
+
+	while len(a) < length:
+		a = np.concatenate((a, [0], np.logical_not(a)[::-1]))
+
+	a = a[:length]
+
+	while len(a) & 1 == 0:
+		a = np.logical_not(np.bitwise_xor(a[::2], a[1::2]))
+
+	return ''.join(map(str, np.array(a, dtype=int)))
+
+
+if __name__ == '__main__':
+	import sys
+	print(main(''.join(sys.stdin.readlines())))
