1d2f62fde769ae6d4ce59c16b53aea61494c27e2
Sanjay Kenn <sanjaykenn@gmail.com>
2021-10-03 16:28:39 +0200

    Adding 2017/16

---
+++ 2017/16/part-1/main.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+
+def main(inp):
+	programs = deque(list(map(chr, range(ord('a'), ord('p') + 1))))
+	for command in inp.rstrip('\n').split(','):
+		if command[0] == 's':
+			programs.rotate(int(command[1:]))
+		elif command[0] == 'x':
+			i1, i2 = map(int, command[1:].split('/'))
+			programs[i1], programs[i2] = programs[i2], programs[i1]
+		elif command[0] == 'p':
+			i1, i2 = map(programs.index, command[1:].split('/'))
+			programs[i1], programs[i2] = programs[i2], programs[i1]
+
+	return ''.join(programs)
+
+
+if __name__ == '__main__':
+	import sys
+	print(main(''.join(sys.stdin.readlines())))
