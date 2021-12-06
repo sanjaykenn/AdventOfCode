267e38160d8b447bf016e2b90b99ae25801f7b8e
Sanjay Kenn <sanjaykenn@gmail.com>
2021-12-06 21:01:07 +0100

    Updating 2021/6

--- 2021/6/part-2/main.py
+++ 2021/6/part-2/main.py
@@ -1,20 +1,15 @@
 def main(inp):
 	days = [0] * 9
-	for d in map(int, inp.rstrip('\n').split(',')):
-		days[d] += 1
+	for day in map(int, inp.rstrip().split(',')):
+		days[day] += 1
 
-	for day in range(256):
-		new_days = [0] * 9
-		new_days[8] = days[0]
-		new_days[6] = days[0]
-		for d in range(8):
-			new_days[d] += days[d + 1]
-
-		days = new_days
+	for day in range(257):
+		days[(day + 6) % 9] += days[(day + 8) % 9]
 
 	return sum(days)
 
 
 if __name__ == '__main__':
 	import sys
+
 	print(main(''.join(sys.stdin.readlines())))
