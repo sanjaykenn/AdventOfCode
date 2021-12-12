3b373ffac7dd29017cb5e4b6550054bd198963e9
Sanjay Kenn <sanjaykenn@gmail.com>
2021-12-12 06:27:53 +0100

    Adding 2021/12

---
+++ 2021/12/part-1/main.py
@@ -0,0 +1,32 @@
+from graph import Graph
+
+
+def dfs(g, start, end='end', visited=None):
+	if start == end:
+		return 1
+
+	visited = visited or set()
+	if start.lower() == start:
+		visited.add(start)
+
+	path_count = 0
+	for _, node, _ in g.edges(from_node=start):
+		if node not in visited:
+			path_count += dfs(g, node, visited=visited.copy())
+
+	return path_count
+
+
+def main(inp):
+	g = Graph()
+
+	for line in inp.splitlines():
+		a, b = line.split('-')
+		g.add_edge(a, b, bidirectional=True)
+
+	return dfs(g, 'start')
+
+
+if __name__ == '__main__':
+	import sys
+	print(main(''.join(sys.stdin.readlines())))
