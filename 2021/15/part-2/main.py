eb3e85f096336d566c756d9050915ca5bce16516
Sanjay Kenn <sanjaykenn@gmail.com>
2021-12-16 16:13:54 +0100

    Updating 2021/15

--- 2021/15/part-2/main.py
+++ 2021/15/part-2/main.py
@@ -6,7 +6,7 @@ def main(inp):
 	a = np.array(list(map(list, inp.splitlines())), int)
 	x, y = np.meshgrid(np.arange(a.shape[1] * 5), np.arange(a.shape[0] * 5))
 	risk = (a[y % a.shape[0], x % a.shape[1]] + y // a.shape[0] + x // a.shape[1] - 1) % 9 + 1
-	g = nx.grid_2d_graph(*risk.shape, create_using=nx.DiGraph)
+	g = nx.grid_2d_graph(*risk.shape)
 	return nx.shortest_path_length(g, (0, 0), (risk.shape[0] - 1, risk.shape[1] - 1), lambda n1, n2, e: risk[n2])
 
 
